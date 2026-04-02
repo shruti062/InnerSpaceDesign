62505271a11b4bc150881a2cc7bb8de1be7e2508
shruti062 <shrutipramanik681@gmail.com>
2026-04-02 12:50:27 +0530

    update

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -7,6 +7,22 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './hero.html',
-  styleUrl: './hero.css',
+  styleUrls: ['./hero.css']
 })
-export class Hero {}
+export class Hero implements AfterViewInit {
+
+  @ViewChild('videoRef') video!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit() {
+    const vid = this.video.nativeElement;
+
+    // Force mute + autoplay (fix browser block issue)
+    vid.muted = true;
+    vid.defaultMuted = true;
+
+    vid.load();
+    vid.play().catch(() => {
+      console.log("Autoplay blocked");
+    });
+  }
+}

23fbfbbf610f70a5fbc1e4bdf4ce9310bfcfad05
shruti062 <shrutipramanik681@gmail.com>
2026-04-16 21:50:14 +0530

    update

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
@@ -17,25 +18,25 @@ export class Contact {
   };
 
   submitForm() {
-  const formDataEncoded = new URLSearchParams({
-    'form-name': 'contact',
-    name: this.formData.name,
-    email: this.formData.email,
-    phone: this.formData.phone,
-    message: this.formData.message
-  });
+    const formDataEncoded = new URLSearchParams({
+      'form-name': 'contact',
+      name: this.formData.name,
+      email: this.formData.email,
+      phone: this.formData.phone,
+      message: this.formData.message
+    });
 
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: formDataEncoded.toString()
-  })
-  .then(() => {
-    alert('Message sent successfully!');
-    this.formData = { name: '', email: '', phone: '', message: '' };
-  })
-  .catch(() => {
-    alert('Something went wrong. Try again!');
-  });
-}
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: formDataEncoded.toString()
+    })
+    .then(() => {
+      alert('Message sent successfully!');
+      this.formData = { name: '', email: '', phone: '', message: '' };
+    })
+    .catch(() => {
+      alert('Something went wrong. Try again!');
+    });
+  }
 }
